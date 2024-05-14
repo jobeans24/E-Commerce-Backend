c935650c762fc446d72e960658ba6ea32eee6cfe
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-05-14 17:12:58 -0400

    leaving comments defining what each function for the connection.js does. this one connects the .env and allows sequelize to run through the information that is within it and access the postgres username

--- code-backend/config/connection.js
+++ code-backend/config/connection.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
+require('dotenv').config(); // require the .env file that is within the code-backend folder
 
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize'); // require the sequelize package
 
-const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PW,
+const sequelize = process.env.DB_URL // if the DB_URL environment variable is present, use that value to connect to the database
+    ? new Sequelize(process.env.DB_URL) // if the DB_URL environment variable is not present, use the values in the .env file to connect to the database
+    : new Sequelize( // create a new Sequelize connection
+        process.env.DB_NAME, // pass in the database name
+        process.env.DB_USER, // pass in the database user
+        process.env.DB_PASSWORD, // pass in the database password
         {
-            host: 'localhost',
-           dialect: 'postgres',
-           dialectOptions: {
-            decimalNumbers: true,
+            host: 'localhost', // use the localhost as the host
+           dialect: 'postgres', // use the postgres dialect
+           dialectOptions: { // set the dialect options
+            decimalNumbers: true, // enable decimal numbers
            }
         });
 
